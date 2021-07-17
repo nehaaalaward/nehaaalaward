6be80a8842f25544469e37f7042f07954d8a4604
Nitesh Kumar Niranjan <hello@theux.dev>
2021-07-17 20:01:59 +0000

    fix: eslint errors

--- pages/index.js
+++ pages/index.js
@@ -18,20 +18,20 @@ export default function HomePage() {
       <div className="hero">
         <div className="container">
           <h1 className="hero-heading">Nehaaal Award.</h1>
-          <p className="description">that's why we care for your worthless(nehaaal) work.</p>
+          <p className="description">that&apos;s why we care for your worthless(nehaaal) work.</p>
 
           <div className="image">
-            <Image  src="/bg.png" height="600" width="846" />
+            <Image src="/bg.png" height="600" width="846" alt="worthless" />
           </div>
 
           <div className="text">
             <h4>Are you worthy for this award?</h4>
-            <button onClick={handleButtonPress}>Let's find out.</button>
+            <button onClick={handleButtonPress}>Let&apos;s find out.</button>
           </div>
 
         </div>
       </div>
-      {isModal && <Modal onClose={() => setIsModal(false)}/>}
+      {isModal && <Modal onClose={() => setIsModal(false)} />}
     </main>
   </div >
 }
